79b503a8476085459fa25b1afae1e3a7cdb8856a
Kamila Zaeva <Kamila_Zaeva@epam.com>
2016-06-27 12:13:25 +0300

    fixed bad realization discount functions

--- src/main/java/com/epam/spring/core/services/impls/discounts/BirthdayStrategy.java
+++ src/main/java/com/epam/spring/core/services/impls/discounts/BirthdayStrategy.java
@@ -6,15 +6,17 @@ import java.util.Date;
 
 public class BirthdayStrategy implements DiscountStrategy{
     
-    private final SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+    private final SimpleDateFormat dayFormat = new SimpleDateFormat("dd");
+    private final SimpleDateFormat monthFormat = new SimpleDateFormat("MM");
+    private final double DISCOUNT_BITHDAY = 0.05;
 
     @Override
     public double getDiscount(User user, Date date) {
         double discount = 0;
-        String day = dateFormat.format(date).substring(0, 2);
-        String month = dateFormat.format(date).substring(3, 5);
-        if (day.equals(user.getBirthday().substring(0, 2)) && month.equals(user.getBirthday().substring(3, 5))) {
-            discount = 0.05;
+        String day = dayFormat.format(date);
+        String month = monthFormat.format(date);
+        if (day.equals(dayFormat.format(user.getBirthday())) && month.equals(monthFormat.format(user.getBirthday()))) {
+            discount = DISCOUNT_BITHDAY;
             System.out.println("User '" + user.getName() + "' had got 5% discount by Birthday Strategy");
         }
         return discount;
--- src/main/java/com/epam/spring/core/services/impls/discounts/EveryTenthStrategy.java
+++ src/main/java/com/epam/spring/core/services/impls/discounts/EveryTenthStrategy.java
@@ -7,12 +7,14 @@ import java.util.List;
 
 public class EveryTenthStrategy implements DiscountStrategy{
     
+    private final double DISCOUNT_TENTH = 0.5;
+    
     @Override
     public double getDiscount(User user, Date date) {
         double discount = 0;
         List<Ticket> purchesedTickets = user.getTickets();
         if (user.getTickets() != null && !purchesedTickets.isEmpty() && purchesedTickets.size()%10 == 0 ) {
-            discount = 0.5;
+            discount = DISCOUNT_TENTH;
             System.out.println("User ' " + user.getName() + "' has got 50% discount by Every 10th Tiket Strategy");
         }
         return discount;
